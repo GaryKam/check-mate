7b9a2bba1c1a88d47898bec9e7a645786be30455
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2023-12-15 16:05:50 -0800

    Adding updated files

---
+++ app/src/main/java/com/oukschub/checkmate/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.oukschub.checkmate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.oukschub.checkmate.ui.theme.CheckMateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CheckMateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Hello world!")
+
+
+                }
+            }
+        }
+    }
+}
