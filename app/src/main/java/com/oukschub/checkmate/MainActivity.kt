cf427daf85f2c709842ca5e9a1c41c679d97c378
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2023-12-21 14:54:40 -0800

    Add FirebaseAuthUI login capability

--- app/src/main/java/com/oukschub/checkmate/MainActivity.kt
+++ app/src/main/java/com/oukschub/checkmate/MainActivity.kt
@@ -2,29 +2,49 @@ package com.oukschub.checkmate
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import com.oukschub.checkmate.ui.theme.CheckMateTheme
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CheckMateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text(text = "Hello world!")
 
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            launchSignIn()
+        } else {
+            startActivity(HomeActivity.createIntent(this))
+            finish()
+        }
+    }
 
-                }
-            }
+    private fun launchSignIn() {
+        val signInLauncher = registerForActivityResult(
+            FirebaseAuthUIActivityResultContract(),
+        ) { result ->
+            onSignInResult(result)
+        }
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.AnonymousBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.CheckMateTheme)
+            .setLogo(com.firebase.ui.auth.R.drawable.abc_btn_check_to_on_mtrl_015)
+            .build()
+
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+            startActivity(HomeActivity.createIntent(this))
+            finish()
         }
     }
 }
