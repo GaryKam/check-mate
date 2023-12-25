93bb2d64dba88ff771e6f90e579a72799d7fb869
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2023-12-24 21:27:12 -0800

    Add sign out button

--- app/src/main/java/com/oukschub/checkmate/ui/screen/Profile.kt
+++ app/src/main/java/com/oukschub/checkmate/ui/screen/Profile.kt
@@ -1,9 +1,13 @@
 package com.oukschub.checkmate.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.firebase.auth.FirebaseAuth
+import com.oukschub.checkmate.SignInActivity
 import com.oukschub.checkmate.ui.component.BottomNavBar
 
 @Composable
@@ -18,7 +22,14 @@ fun Profile(
         onNavigateToProfile = { /*TO-DO*/ }
     ) {
         Column {
+            val context = LocalContext.current
             Text(text = "Profile")
+            Button(onClick = {
+                FirebaseAuth.getInstance().signOut()
+                context.startActivity(SignInActivity.createIntent(context))
+            }) {
+                Text(text = "Sign Out")
+            }
         }
     }
 }
