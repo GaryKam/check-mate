7f21fc25f159bd002a457ee5820ef9133aba988f
Gary Kam <garyhkam@gmail.com>
2023-12-31 15:39:51 -0800

    Basic splash screen

--- app/src/main/java/com/oukschub/checkmate/MainActivity.kt
+++ app/src/main/java/com/oukschub/checkmate/MainActivity.kt
@@ -3,12 +3,15 @@ package com.oukschub.checkmate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.auth.FirebaseAuth
 import com.oukschub.checkmate.navigation.CheckMateApp
 import com.oukschub.checkmate.ui.theme.CheckMateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         setContent {
