ecd6d0a7f38550a3a342c965a7f91498b53f6591
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2024-01-02 21:55:03 -0800

    Rearrange sign in screen

--- app/src/main/java/com/oukschub/checkmate/ui/screen/SignIn.kt
+++ app/src/main/java/com/oukschub/checkmate/ui/screen/SignIn.kt
@@ -1,14 +1,19 @@
 package com.oukschub.checkmate.ui.screen
 
-import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,12 +23,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.oukschub.checkmate.R
+import com.oukschub.checkmate.util.MessageUtil
 import com.oukschub.checkmate.viewmodel.SignInViewModel
 
 @Composable
@@ -38,13 +49,65 @@ fun SignIn(
 
     Column(
         modifier = modifier
-            .fillMaxSize()
-            .padding(20.dp),
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .weight(.85F, true),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Logo()
+
+            InputFields(
+                email = email,
+                password = password,
+                onChangeEmail = { email = it },
+                onChangePassword = { password = it },
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            SignInButton(
+                viewModel = viewModel,
+                email = email,
+                password = password,
+                onSignIn = onSignIn
+            )
+        }
+
+        Footer(
+            onClickSignUp = onClickSignUp,
+            modifier = Modifier.weight(.15F, true)
+        )
+    }
+}
+
+@Composable
+private fun Logo() {
+    Image(
+        painter = painterResource(R.drawable.ic_launcher_background),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(30.dp)
+            .size(200.dp)
+            .border(BorderStroke(4.dp, MaterialTheme.colorScheme.primary), CircleShape)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+private fun InputFields(
+    email: String,
+    password: String,
+    onChangeEmail: (String) -> Unit,
+    onChangePassword: (String) -> Unit,
+) {
+    Column {
         OutlinedTextField(
             value = email,
-            onValueChange = { email = it },
+            onValueChange = { onChangeEmail(it) },
             placeholder = { Text(text = stringResource(R.string.email)) }
         )
 
@@ -52,43 +115,54 @@ fun SignIn(
 
         OutlinedTextField(
             value = password,
-            onValueChange = { password = it },
+            onValueChange = { onChangePassword(it) },
             placeholder = { Text(text = stringResource(R.string.password)) }
         )
+    }
+}
 
-        Spacer(modifier = Modifier.height(10.dp))
+@Composable
+private fun SignInButton(
+    viewModel: SignInViewModel,
+    email: String,
+    password: String,
+    onSignIn: () -> Unit,
+) {
+    val context = LocalContext.current
 
-        val context = LocalContext.current
-        Button(
-            onClick = {
-                viewModel.signIn(
-                    email = email,
-                    password = password,
-                    onSuccess = { onSignIn() },
-                    onFailure = {
-                        Toast.makeText(
-                            context,
-                            R.string.sign_in_failure,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    },
-                    onError = {
-                        Toast.makeText(
-                            context,
-                            R.string.sign_in_error,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                )
-            }
-        ) {
-            Text(text = stringResource(R.string.sign_in))
+    Button(
+        onClick = {
+            viewModel.signIn(
+                email = email,
+                password = password,
+                onSuccess = { onSignIn() },
+                onFailure = { MessageUtil.displayToast(context, R.string.sign_in_failure) },
+                onError = { MessageUtil.displayToast(context, R.string.sign_in_error) }
+            )
         }
+    ) {
+        Text(text = stringResource(R.string.sign_in))
+    }
+}
 
-        Divider(Modifier.padding(vertical = 50.dp))
+@Composable
+private fun Footer(
+    onClickSignUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Divider(Modifier.padding(vertical = 30.dp))
 
         ClickableText(
-            text = AnnotatedString(text = stringResource(R.string.sign_in_prompt_to_sign_up)),
+            text = buildAnnotatedString {
+                append(stringResource(R.string.sign_in_prompt_to_sign_up))
+                append(" ")
+                pushStyle(SpanStyle(color = Color.Blue, fontWeight = FontWeight.Bold))
+                append(stringResource(R.string.sign_up))
+            },
             onClick = { onClickSignUp() }
         )
     }
---
+++ app/src/main/java/com/oukschub/checkmate/util/MessageUtil.kt
@@ -0,0 +1,18 @@
+package com.oukschub.checkmate.util
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+object MessageUtil {
+    fun displayToast(
+        context: Context,
+        @StringRes resId: Int
+    ) {
+        Toast.makeText(
+            context,
+            resId,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
