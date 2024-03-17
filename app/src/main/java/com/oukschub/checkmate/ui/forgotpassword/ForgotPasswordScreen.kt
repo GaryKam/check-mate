202a63972b89609cb79aeab0ec31b3133f248922
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2024-03-16 19:01:17 -0700

    Add back button to forgot password screen

--- app/src/main/java/com/oukschub/checkmate/ui/forgotpassword/ForgotPasswordScreen.kt
+++ app/src/main/java/com/oukschub/checkmate/ui/forgotpassword/ForgotPasswordScreen.kt
@@ -3,8 +3,15 @@ package com.oukschub.checkmate.ui.forgotpassword
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +24,7 @@ import com.oukschub.checkmate.R
 import com.oukschub.checkmate.ui.component.EmailTextField
 import com.oukschub.checkmate.util.MessageUtil
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ForgotPasswordScreen(
     onBack: () -> Unit,
@@ -30,29 +38,49 @@ fun ForgotPasswordScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        EmailTextField(
-            email = viewModel.email,
-            errorMessage = stringResource(viewModel.emailError),
-            focusManager = focusManager,
-            onEmailChange = { viewModel.changeEmail(it) },
-            modifier = Modifier.padding(top = 80.dp)
-        )
-
-        val context = LocalContext.current
-        Button(
-            onClick = {
-                focusManager.clearFocus()
-                viewModel.resetPassword(
-                    onSuccess = {
-                        onBack()
-                        MessageUtil.displayToast(context, R.string.forgot_password_success)
-                    },
-                    onFailure = { MessageUtil.displayToast(context, R.string.forgot_password_failure) }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {},
+                    navigationIcon = {
+                        IconButton(onClick = onBack) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = stringResource(R.string.desc_back)
+                            )
+                        }
+                    }
                 )
-            },
-            modifier = Modifier.align(alignment = Alignment.End)
-        ) {
-            Text("Send")
+            }
+        ) { paddingValues ->
+            Column {
+                EmailTextField(
+                    email = viewModel.email,
+                    errorMessage = stringResource(viewModel.emailError),
+                    focusManager = focusManager,
+                    onEmailChange = { viewModel.changeEmail(it) },
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .padding(top = 15.dp)
+                )
+
+                val context = LocalContext.current
+                Button(
+                    onClick = {
+                        focusManager.clearFocus()
+                        viewModel.resetPassword(
+                            onSuccess = {
+                                onBack()
+                                MessageUtil.displayToast(context, R.string.forgot_password_success)
+                            },
+                            onFailure = { MessageUtil.displayToast(context, R.string.forgot_password_failure) }
+                        )
+                    },
+                    modifier = Modifier.align(alignment = Alignment.End)
+                ) {
+                    Text("Send")
+                }
+            }
         }
     }
 }
